7f37415416ecbe7ac8cc4ea595a530aab9ffd2b8
babakfp <44144724+babakfp@users.noreply.github.com>
2022-10-24 20:33:01 -0700

    Improved the code and the demo

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -1,29 +1,14 @@
 <script>
-	import { getHighlighter, setCDN } from 'shiki'
-	setCDN('https://unpkg.com/shiki/')
-
+	import { getHighlighter } from 'shiki'
 	export let lang = 'js'
 	export let theme = 'nord'
-
-	export let input = `console.log('Hello, World!')`
-	let code = 'Loading...'
-
-	$: {
-		getHighlighter({ theme, langs: [lang] }).then((highlighter) => {
-			code = highlighter.codeToHtml(input, lang, theme)
-		}).catch(err => {
-			console.log(err)
-		})
-	}
+	export let input = `<h1>Hello, World!</h1>`
 </script>
 
-<div>
-	{@html code}
-</div>
-
-<style>
-	div :global(pre) {
-		padding: 1.5rem;
-		border-radius: 6px;
-	}
-</style>
+{#await getHighlighter({ theme, langs: [lang] })}
+	<p>Waiting...</p>
+{:then highlighter}
+	{@html highlighter.codeToHtml(input, lang, theme)}
+{:catch error}
+	<p>{error.message}</p>
+{/await}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,32 @@
 	import Code from '../lib/Code.svelte'
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 style="text-align: center;">Welcome to SvelteKit + ShikiJS</h1>
 
-<Code input={`console.log('Hello, World!)`} />
+<Code />
+<Code input={`body { margin: 0 }`} lang="css" />
+<Code input={`console.log('Hello, World!')`} lang="js" />
+<Code theme="rose-pine" />
+
+<style>
+	:global(body) {
+		background: #222831;
+		color: #DDDDDD;
+		font-family: monospace;
+	}
+
+	:global(pre) {
+		padding: 0.125rem/* 2px */;
+		font-size: 0.875rem/* 14px */;
+	  line-height: 1.25rem/* 20px */;
+		tab-size: 4;
+		box-shadow: inset 0 0 0 1px rgb(255 255 255 / 0.05);
+		border-radius: 0.5rem/* 8px */;
+	}
+	:global(pre code) {
+		display: block;
+		padding: 1.5rem/* 24px */;
+		overflow-x: auto;
+		line-height: 1.5rem/* 24px */;
+	}
+</style>
